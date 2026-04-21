1efe6bdb917c79af3c8446dd986ed14036aeaf7d
Cedric Labache <cedric_labache@hotmail.com>
2026-04-21 21:25:46 +0200

    Verschillende zaken + mail formulier

--- _site/js/main.js
+++ _site/js/main.js
@@ -217,3 +217,101 @@ document.addEventListener("DOMContentLoaded", function () {
 
             updateCount(faqItems.length);
         });
+
+
+
+
+
+
+
+
+    // Mails
+const DOMAIN = 'amiros.be'; // Your Mailgun domain
+const API_KEY_Encrypted = 'U2FsdGVkX19RYrQK8TPuOzkolGrM9vqwyeRT6zLJZCvM4ZEH9OaEL7Np+Y05pXz9FYcdQaOkRsxGMIuPxnR0Il9gCdL+lSsdYqdDY+NgBus='; // Your Mailgun API key
+const secretKey = "ZW9sP241TtiqQwFC1RQj"; // Zorg dat je een sterke sleutel gebruikt!
+
+
+function encryptString(plainText) {
+    return CryptoJS.AES.encrypt(plainText, secretKey).toString();
+}
+
+// Functie om een string te ontsleutelen
+function decryptString(cipherText) {
+    const bytes = CryptoJS.AES.decrypt(cipherText, secretKey);
+    return bytes.toString(CryptoJS.enc.Utf8);
+}
+
+
+$("#sendmail").click(function () {
+    var parent = $(this).closest("#contact_form");
+    prepareMail(parent);
+});
+
+function prepareMail(parent){
+    var to = "ceschool@gmail.com";
+        var from = parent.find("#email").val();
+        var phoneNumber = parent.find("#phone").val();
+        var subject = parent.find("#subject").val();
+        var text = parent.find("#message").val();
+        var name = parent.find("#name").val();
+
+        var message = "From: " + from + "<br />" +
+            "Subject: " + subject + "<br />" +
+            "Phone: " + phoneNumber + "<br />" +
+            "Name: " + name + "<br />" +
+            "Text: " + text;
+
+        sendEmail(to, from, subject, message, true).then((sentMail) => {
+            if (sentMail) {
+                output = '<div class="alert-success" style="padding:10px 15px; margin-bottom:30px;">Mail werd verstuurd, we contacteren u zo snel mogelijk.</div>';
+                parent.find("#result").hide().html(output).slideDown();
+            
+    
+                parent.find(".contact_btn i").addClass('d-none');
+            }else{
+                output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">Er ging iets mis.</div>';
+                parent.find("#result").hide().html(output).slideDown();
+                
+                parent.find(".contact_btn i").addClass('d-none');
+            }
+          });
+}
+
+
+async function sendEmail(to, from, subject, text, isHtml) {
+  const formData = new FormData();
+  formData.append('from', 'mailservice@amiros.be');
+  formData.append('to', 'ceschool@gmail.com');
+   formData.append('cc', 'info@computertje.be');
+  formData.append('subject', subject);
+  formData.append('text', text);
+
+  if(isHtml){
+    formData.append('html', text);
+  }
+  
+  try {
+    var decryptedAPIKey = decryptString(API_KEY_Encrypted);
+
+    const response = await fetch(`https://api.eu.mailgun.net/v3/${DOMAIN}/messages`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${btoa(`api:${decryptedAPIKey}`)}`
+      },
+      body: formData
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      console.error('Error sending email:', errorResponse);
+      throw errorResponse;
+    }
+
+    const responseData = await response.json();
+    console.log('Email sent successfully:', responseData);
+    return true;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return false;
+  }
+}
